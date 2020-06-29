31bfda8ae46e85af2817d9a720eeb8cf91cb0007
Paolo Congco <pcongco@gmail.com>
2020-06-29 17:10:50 +0800

    Exercise 1.7 unicafe: modify application to display more statistics about feedback - total feedback, average score, percentage of positive feedback

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,60 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const Header = (props) => {
-  return (
-    <h1>{props.course}</h1>
-  )
-}
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
-const Content = (props) => {
+const Feedback = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  // Score equivalent: Good: 1, Neutral: 0, Bad: -1
+  const average = (good + (bad * -1)) / total
+  const positive = (good / total) * 100
+  
   return (
-    <>
-      <Part part={props.parts[0]} />
-      <Part part={props.parts[1]} />
-      <Part part={props.parts[2]} />
-    </>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.part.name} {props.part.exercises}
-    </p>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
+    <div>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>All: {total}</p>
+      <p>Average: {average}</p>
+      <p>Positive: {positive}%</p>
+    </div>
   )
 }
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-      name: 'Fundamentals of React',
-      exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises:  7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts}/>
-      <Total parts={course.parts} />
+      <h1>Give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+
+      <h1>Statistics</h1>
+      <Feedback good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
