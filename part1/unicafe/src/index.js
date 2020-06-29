706d1d941863527865afd2eaa3a5be45e4404567
Paolo Congco <pcongco@gmail.com>
2020-06-29 16:57:26 +0800

    Exercise 1.6 unicafe: add initial application, display collected feedback for each category

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Feedback = ({ good, neutral, bad }) => (
+    <div>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+    </div>
+  )
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button handleClick={() => setGood(good + 1)} text='good' />
+      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
+      <Button handleClick={() => setBad(bad + 1)} text='bad' />
+
+      <h1>Statistics</h1>
+      <Feedback good={good} neutral={neutral} bad={bad} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
