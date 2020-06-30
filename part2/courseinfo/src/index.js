f09504c73e7f3b2aa94b95a0ab0df0f810091600
Paolo Congco <pcongco@gmail.com>
2020-06-30 15:20:22 +0800

    Exercise 2.5 course information: declare the Course component as a separate module which is imported by the App component

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,48 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Header = ({ course }) => {
-  return (
-    <h2>{course.name}</h2>
-  )
-}
-
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>    
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map((part) => 
-        <Part key={part.id} name={part.name} exercises={part.exercises} />
-      )}
-    </div>
-  )
-}
-
-const Total = ({ course }) => {
-  // To sum up values in an array of objects, an initial value must be supplied first
-  const total = course.parts.reduce((sum, part) => sum + part.exercises, 0)
-
-  return (
-    <p><b>total of {total} exercises</b></p>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>    
-  )
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
