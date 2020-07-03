c553750b3d07640b89a4296290ecd9905e57c1c0
Paolo Congco <pcongco@gmail.com>
2020-07-03 18:01:09 +0800

    Exercise 3.2 phonebook backend: Implement a page at /info that shows how many entries are in the phonebook at the time of the request and shows the time the request was received.

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -28,6 +28,13 @@ app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+  const numPersons = persons.length
+  const info = `Phonebook has info for ${numPersons} people`
+  
+  response.send(`<div>${info}</div><br /><div>${new Date()}</div>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
