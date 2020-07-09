043a5c51bceac08fdb1721c8b649c6f9828c25ec
Paolo Congco <pcongco@gmail.com>
2020-07-09 20:34:00 +0800

    Commit Part 1 exercise Unicafe into the correct directory. It was committed at the wrong directory (part1/courseinfo) during Exercise 1.7 - 1.11. Restore the overwritten part1 courseinfo file to the correct state during part 1

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,62 +1,60 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
-  </button>
-)
-
-const Statistic = (props) => (
-  <tr>
-    <td>{props.text}</td>
-    <td>{props.value}</td>
-  </tr>
-)
-
-const Statistics = ({ good, neutral, bad }) => {
-  const total = good + neutral + bad
-  // Score equivalent: Good: 1, Neutral: 0, Bad: -1
-  const average = (good + (bad * -1)) / total
-  const positive = (good / total) * 100
-
-  // Display statistics only once feedback has been gathered
-  if (total === 0) {
-    return (
-      <p>No feedback given</p>
-    )
-  }
-
+const Header = (props) => {
   return (
-    <div>
-      <h1>Statistics</h1>
-      <table>
-        <tbody>
-          <Statistic text="Good" value={good} />
-          <Statistic text="Neutral" value={neutral} />
-          <Statistic text="Bad" value={bad} />
-          <Statistic text="All" value={total} />
-          <Statistic text="Average" value={average} />
-          <Statistic text="Positive" value={positive + "%"} />
-        </tbody>
-      </table>
-    </div>
+    <h1>{props.course}</h1>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.parts[0]} />
+      <Part part={props.parts[1]} />
+      <Part part={props.parts[2]} />
+    </>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   )
 }
 
 const App = () => {
-  // save clicks of each button to own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+      name: 'Fundamentals of React',
+      exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises:  7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <h1>Give feedback</h1>
-      <Button handleClick={() => setGood(good + 1)} text='good' />
-      <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
-      <Button handleClick={() => setBad(bad + 1)} text='bad' />
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Header course={course.name} />
+      <Content parts={course.parts}/>
+      <Total parts={course.parts} />
     </div>
   )
 }
--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -7,13 +7,42 @@ const Button = (props) => (
   </button>
 )
 
-const Feedback = ({ good, neutral, bad }) => (
+const Statistic = (props) => (
+  <tr>
+    <td>{props.text}</td>
+    <td>{props.value}</td>
+  </tr>
+)
+
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  // Score equivalent: Good: 1, Neutral: 0, Bad: -1
+  const average = (good + (bad * -1)) / total
+  const positive = (good / total) * 100
+
+  // Display statistics only once feedback has been gathered
+  if (total === 0) {
+    return (
+      <p>No feedback given</p>
+    )
+  }
+
+  return (
     <div>
-      <p>Good: {good}</p>
-      <p>Neutral: {neutral}</p>
-      <p>Bad: {bad}</p>
+      <h1>Statistics</h1>
+      <table>
+        <tbody>
+          <Statistic text="Good" value={good} />
+          <Statistic text="Neutral" value={neutral} />
+          <Statistic text="Bad" value={bad} />
+          <Statistic text="All" value={total} />
+          <Statistic text="Average" value={average} />
+          <Statistic text="Positive" value={positive + "%"} />
+        </tbody>
+      </table>
     </div>
   )
+}
 
 const App = () => {
   // save clicks of each button to own state
@@ -27,9 +56,7 @@ const App = () => {
       <Button handleClick={() => setGood(good + 1)} text='good' />
       <Button handleClick={() => setNeutral(neutral + 1)} text='neutral' />
       <Button handleClick={() => setBad(bad + 1)} text='bad' />
-
-      <h1>Statistics</h1>
-      <Feedback good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
