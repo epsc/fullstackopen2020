83fd9fab02500c57984acd9818d62b35b7ed316f
Paolo Congco <pcongco@gmail.com>
2020-07-09 18:56:16 +0800

    Exercise 4.4 helper functions and unit tests: Define a new totalLikes function that reeives a list of blog posts as a parameter and returns the total sum of likes in all of the blog posts. Write appropriate tests for the function. Also, correct the typo error (incomplete toBe function) of dummy test from previous commit

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, value) => {
+    return sum + value
+  }
+
+  return blogs.map(blog => blog.likes).reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
