9dae710b28e6848ced2e2cd8991837f814b15f6f
Paolo Congco <pcongco@gmail.com>
2020-07-10 18:39:57 +0800

    Exercise 4.10 blog list tests: Write a test that verifies that making an HTTP POST request to the /api/blogs url successfully creates a new blog post. Verify that the total number of blogs has increased by one. Also verify that the content of the blog post is saved correctly to the database

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -36,6 +36,30 @@ test('blog entry id is defined as id, not as _id', async () => {
   expect(retrievedBlog._id).not.toBeDefined()
 })
 
+test('blog entry is successfully created', async () => {
+  const newBlog = {
+    title: 'A new blog post',
+    author: 'VS Code Rest Client',
+    url: 'http://newblogpost.com/newPost',
+    likes: 2,
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  // last index is of array is length - 1. New array length is + 1 so last index is now equal to previous length value
+  const addedBlog = response.body[helper.initialBlogs.length]
+
+  expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+  expect(addedBlog).toHaveProperty('title', 'A new blog post')
+  expect(addedBlog).toHaveProperty('author', 'VS Code Rest Client')
+  expect(addedBlog).toHaveProperty('url', 'http://newblogpost.com/newPost')
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
