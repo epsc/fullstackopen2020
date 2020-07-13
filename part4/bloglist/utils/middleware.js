9292d1510a497159a9d16d7f22f36d6a18e12934
Paolo Congco <pcongco@gmail.com>
2020-07-13 20:41:01 +0800

    Exercise 4.20 bloglist expansion: Refactor taking the token to a middleware. The middleware should take the token from the authorization header and place it to the token field of the request object

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -13,4 +13,15 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
-module.exports = { errorHandler }
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    request.token = authorization.substring(7)
+  } else {
+    request.token = null
+  }
+
+  next()
+}
+
+module.exports = { errorHandler, tokenExtractor }
