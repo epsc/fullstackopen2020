628acc88ba769e32415d46cf338b63aeb19058be
Paolo Congco <pcongco@gmail.com>
2020-07-15 17:14:24 +0800

    Exercise 5.7 bloglist frontend: Add a button for each blog that controls if all of the details about the blog are shown or not. Full details of the blog open when the button is clicked, and the details are hidden when the button is clicked again. Also, add some inline css to blogs.

--- part5/bloglist_frontend/src/components/Blog.js
+++ part5/bloglist_frontend/src/components/Blog.js
@@ -1,8 +1,40 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const showWhenVisible = { display: visible ? '' : 'none' }
+  const viewButtonLabel = visible ? 'hide' : 'view'
+
+  const blogStyle = {
+    border: 'solid',
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 5,
+    padding: 5
+  }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const username = blog.user ? blog.user.name : "No user"
+  return (
+  <div style={blogStyle}>
+    <div>
+      {blog.title} {blog.author}
+      <button onClick={toggleVisibility}>{viewButtonLabel}</button>
+    </div>
+
+    <div style={showWhenVisible}>
+      {blog.url} 
+      <br />
+      likes {blog.likes} <button>like</button>
+      <br />
+      {username}
+    </div>
   </div>
-)
+  )
+}
 
 export default Blog
