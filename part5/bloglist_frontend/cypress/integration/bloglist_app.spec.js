41388386a4b60a5ea71d1541c778ca7fdbd9ba42
Paolo Congco <pcongco@gmail.com>
2020-07-17 18:28:54 +0800

    Exercise 5.18 bloglist end to end testing: Make tests for logging in, testing both successful and unsuccessful log in attempts. Make a user in the beforeEach block for the tests. Also, check that the notification shown with unsuccessful login is displayed red

--- part5/bloglist_frontend/cypress/integration/bloglist_app.spec.js
+++ part5/bloglist_frontend/cypress/integration/bloglist_app.spec.js
@@ -1,6 +1,12 @@
 describe('Blog app', function() {
   beforeEach(function() {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    const user = {
+      name: 'Juan Dela Cruz',
+      username: 'jdlc',
+      password: 'password'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
     cy.visit('http://localhost:3000')
   })
 
@@ -9,5 +15,24 @@ describe('Blog app', function() {
     cy.contains('username')
     cy.contains('password')
     cy.contains('login')
+  })
+
+  describe('login', function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('[name="Username"]').type('jdlc')
+      cy.get('[name="Password"]').type('password')
+      cy.contains('login').click()
+
+      cy.contains('Juan Dela Cruz logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('[name="Username"]').type('jdlc')
+      cy.get('[name="Password"]').type('wrong password')
+      cy.contains('login').click()
+
+      cy.get('.error').should('contain', 'Invalid username or password')
+        .and('have.css', 'color', 'rgb(255, 0, 0)')
+    })
   })
 })
