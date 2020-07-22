a0d0c1687f5156928cfaff7eaffb62dbf3cf78a7
Paolo Congco <pcongco@gmail.com>
2020-07-22 17:18:14 +0800

    Exercise 6.21 anecdotes, the grand finale: Fix bug so that after multiple votes in a row, the notification for the last vote is displayed for five seconds, done by cancelling the removal of the previous notification when a new notification is displayed whenever necessary

--- part6/redux_anecdotes/src/reducers/notificationReducer.js
+++ part6/redux_anecdotes/src/reducers/notificationReducer.js
@@ -1,6 +1,9 @@
 const notificationReducer = (state = '', action) => {
   switch (action.type) {
     case 'SET NOTIFICATION':
+      // first cancel the removal of previous notification if any
+      clearTimeout(state.timeoutID)
+
       state = action.data
       return state
     case 'REMOVE NOTIFICATION':
@@ -14,14 +17,19 @@ const notificationReducer = (state = '', action) => {
 export const setNotification = (notification, timeoutInSeconds) => {
   return async dispatch => {
     const timeout = timeoutInSeconds * 1000
-    dispatch({
-      type: 'SET NOTIFICATION',
-      data: notification
-    })
-    setTimeout(() => 
+    // store timeout id of removeNotification to state store so we can cancel if necessary
+    const timeoutID = setTimeout(() => 
       dispatch(removeNotification()), 
       timeout
     )
+
+    dispatch({
+      type: 'SET NOTIFICATION',
+      data: {
+        notification,
+        timeoutID
+      }
+    })
   }
 }
 
