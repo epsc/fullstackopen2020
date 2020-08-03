cd5030389a86f2cc6207e638f13fc0c60d36a254
Paolo Congco <pcongco@gmail.com>
2020-08-03 17:25:13 +0800

    Exercise 8.6 adding a book: Implement mutation addBook. The mutation works even if the author is not already saved to the server. If the author is not yet saved to the server, a new author is added to the system.

--- part8/library_backend/index.js
+++ part8/library_backend/index.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { v4: uuidv4 } = require('uuid')
 
 let authors = [
   {
@@ -108,6 +109,15 @@ const typeDefs = gql`
     born: Int
     bookCount: Int!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -136,6 +146,19 @@ const resolvers = {
   },
   Author: {
     bookCount: (root) => books.filter(book => book.author === root.name).length
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuidv4() }
+      books = books.concat(book)
+
+      // Add author to authors list if the author of the book is not yet in the list
+      if (!authors.map(author => author.name).includes(args.author)) {
+        authors = authors.concat({ name: args.author, id: uuidv4() })
+      }
+
+      return book
+    }
   }
 }
 
