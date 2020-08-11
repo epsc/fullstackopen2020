2afe9645e2fc2d5f660d4b2413841cb1d56a7b72
Paolo Congco <pcongco@gmail.com>
2020-08-11 18:13:05 +0800

    Exercise 9.1 Body mass index: In a bmiCalculator.ts file, write a function calculateBmi that counts BMI based on given weight (in kilograms) and height (in centimeters) and then returns a message that suits the results. Call the function in the same file with hard-coded parameters and print out the result. Create a npm script for running the program with command nmp run calculateBmi

---
+++ part9/exercises_1to7/bmiCalculator.ts
@@ -0,0 +1,29 @@
+const calculateBMI = (heightInCm: number, weightInKg: number) : string => {
+  if (heightInCm <= 0 || weightInKg <= 0) {
+    return 'Invalid height or weight'
+  }
+
+  const bmi =  weightInKg / Math.pow(heightInCm / 100, 2);
+  let result;
+  
+  if (bmi < 15) {
+    result = 'Very severely underweight';
+  } else if (bmi < 16) {
+    result = 'Severely underweight';
+  } else if (bmi < 18.5) {
+    result = 'Underweight';
+  } else if (bmi < 25) {
+    result = 'Normal (healthy weight)';
+  } else if (bmi < 30) {
+    result = 'Overweight';
+  } else if (bmi < 35) {
+    result = 'Obese Class I (Moderately Obese)';
+  } else if (bmi < 40) {
+    result = 'Obese Class II (Severely Obese)';
+  } else if (bmi > 40) {
+    result = 'Very severely obese';
+  }
+  return result;
+}
+
+console.log(calculateBMI(180, 74));
