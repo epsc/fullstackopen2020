3baabc9b175bb3e9c91604cedc5c3b7a8e8dea4c
Paolo Congco <pcongco@gmail.com>
2020-08-13 16:13:04 +0800

    Exercise 9.9 Patientor backend: Ensure that backend answers to the ping request that cloned frontend has made on startup. Check developer tool to make sure it really works.

--- part9/patientor_backend/index.ts
+++ part9/patientor_backend/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+
 
 const PORT = 3001;
 
-app.get('/ping', (_request, response) => {
+app.get('/api/ping', (_request, response) => {
   response.send('pong');
 });
 
