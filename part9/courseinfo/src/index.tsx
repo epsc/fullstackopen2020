594e6d204cd135ae6af0d797aebce20a9ebc5d85
Paolo Congco <pcongco@gmail.com>
2020-08-17 16:02:21 +0800

    Exercise 9.15 course info: Add the provided type information and replace the variable courseParts with the one provided. Declare a new interface that includes the description attribute and extends the CoursePartBase interface. Then modify the code so that the description attribute can be removed from both CoursePartOne and CoursePartThree without getting any errors. Create a component Part that renders all attributes of each type of course part. Use a switch case -based exhaustive type checking.  Use the new component in compontent Content. Add own course part interface with at least name, exerciseCount and description attributes then add that interface to type union CoursePart and add corresponding data to the courseParts variable. Make the necessary changes so that all attributes for the new course part also get rendered and that the compiler does not produce any errors.

--- part9/courseinfo/src/index.tsx
+++ part9/courseinfo/src/index.tsx
@@ -9,27 +9,106 @@ const Header: React.FC<HeaderProps> = ({ courseName }) => (
   <h1>{courseName}</h1>
 );
 
-interface CoursePart {
+interface CoursePartBase {
   name: string;
   exerciseCount: number;
 }
+
+interface CoursePartWithDescription extends CoursePartBase {
+  description: string;
+}
+
+interface CoursePartOne extends CoursePartWithDescription {
+  name: "Fundamentals";
+}
+
+interface CoursePartTwo extends CoursePartBase {
+  name: "Using props to pass data";
+  groupProjectCount: number;
+}
+
+interface CoursePartThree extends CoursePartWithDescription {
+  name: "Deeper type usage";
+  exerciseSubmissionLink: string;
+}
+
+interface CoursePartOwn extends CoursePartWithDescription {
+  name: "Adding own course part";
+  additionalComment: string;
+}
+
+type CoursePart = CoursePartOne | CoursePartTwo | CoursePartThree | CoursePartOwn;
 
 interface CoursePartProps {
   courseParts: Array<CoursePart>;
 }
 
+interface PartProps {
+  part: CoursePart;
+}
+
+const Part: React.FC<PartProps> = ({ part }) => {
+  const assertNever = (value: never): never => {
+    throw new Error(
+      `Unhandled discriminated union member: ${JSON.stringify(value)}`
+    );
+  };
+
+  switch (part.name) {
+    case "Fundamentals":
+      return (
+        <div>
+          <h3>{part.name}</h3>
+          Exercise count: {part.exerciseCount}
+          <br />
+          Description: {part.description}
+        </div>
+      );
+    case "Using props to pass data":
+      return (
+        <div>
+          <h3>{part.name}</h3>
+          Exercise count: {part.exerciseCount}
+          <br />
+          Group project count: {part.groupProjectCount}
+        </div>
+      );
+    case "Deeper type usage":
+      return (
+        <div>
+          <h3>{part.name}</h3>
+          Exercise count: {part.exerciseCount}
+          <br />
+          Description: {part.description}
+          <br />
+          Exercise submission link: <a href={part.exerciseSubmissionLink}>{part.exerciseSubmissionLink}</a>
+        </div>
+      );
+      case "Adding own course part":
+        return (
+          <div>
+            <h3>{part.name}</h3>
+            Exercise count: {part.exerciseCount}
+            <br />
+            Description: {part.description}
+            <br />
+            Additional comment: {part.additionalComment}
+          </div>
+        );
+    default:
+      return assertNever(part);
+
+  }
+}
+
 const Content: React.FC<CoursePartProps> = ({ courseParts }) => {
   return (
     <div>
-      <p>
-        {courseParts[0].name} {courseParts[0].exerciseCount}
-      </p>
-      <p>
-        {courseParts[1].name} {courseParts[1].exerciseCount}
-      </p>
-      <p>
-        {courseParts[2].name} {courseParts[2].exerciseCount}
-      </p>
+      <div>
+        {courseParts.map(part => 
+          <Part key={part.name} part={part} />
+        )}
+      </div>
     </div>
   )
 };
@@ -37,7 +116,7 @@ const Content: React.FC<CoursePartProps> = ({ courseParts }) => {
 const Total: React.FC<CoursePartProps> = ({ courseParts }) => {
   return (
     <p>
-    Number of exercises{" "}
+    Total Number of exercises{" "}
     {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
     </p>
   );
@@ -45,18 +124,28 @@ const Total: React.FC<CoursePartProps> = ({ courseParts }) => {
 
 const App: React.FC = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
+      exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev"
+    },
+    {
+      name: "Adding own course part",
+      exerciseCount: 1,
+      description: "For exercise 9.15",
+      additionalComment: "I hope it's correct"
     }
   ];
 
